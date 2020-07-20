55453c438ebee74e8f40098095c706b53154e432
vsidera <siderravictor@gmail.com>
2020-07-20 13:56:27 +0300

    do code indentation and spacing

--- js/script.js
+++ js/script.js
@@ -1,26 +1,19 @@
-// Backend
-var akan = function(y, m, d, g) {
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var d = new Date(y, --m, d);
-    if (g === "Female") {
+//backend
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var d = new Date(y, --m, d);
+if (g === "Female") {
         return d && femaleNames[d.getDay()];
     } else {
         return d && maleNames[d.getDay()];
     }
 }
 
-// Front End
-$(document).ready(function() {
-    $("form#form").submit(function(event) {
-        event.preventDefault();
-        var y = parseInt($("#year").val());
-        var m = parseInt($("#month").val());
-        var d = parseInt($("#date").val());
-        var g = $("input:radio[name=gender]:checked").val();
-        var result = akan(y, m, d, g);
-        alert("Your akan name is: " + result);
-        //Refresh
-        document.getElementById("form").reset();
+//front-end
+$(document).ready ( function () {
+    $("#form").submit(function () {
+      event.preventDefault();
+       alert("Your name is Kwame");
+       document.getElementById("form").reset();
     });
 });
