f6f27e6bc4d48c670dd91fa7633cec566cb7a8e4
vsidera <siderravictor@gmail.com>
2020-07-20 14:00:51 +0300

    add the documen.ready function to js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
-//backend
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var d = new Date(y, --m, d);
-if (g === "Female") {
+// Backend
+var akan = function(y, m, d, g) {
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var d = new Date(y, --m, d);
+    if (g === "Female") {
         return d && femaleNames[d.getDay()];
     } else {
         return d && maleNames[d.getDay()];
     }
 }
 
-//front-end
-$(document).ready ( function () {
-    $("#form").submit(function () {
-      event.preventDefault();
-       alert("Your name is Kwame");
-       document.getElementById("form").reset();
+// Frontend
+$(document).ready(function() {
+    $("form#form").submit(function(event) {
+        event.preventDefault();
+        var y = parseInt($("#year").val());
+        var m = parseInt($("#month").val());
+        var d = parseInt($("#date").val());
+        var g = $("input:radio[name=gender]:checked").val();
+        var result = akan(y, m, d, g);
+        alert("Your akan name is: " + result);
+        //refresh page
+        document.getElementById("form").reset();
     });
 });
